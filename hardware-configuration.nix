fc16a4ee831bb76e026cdff69b5d14175dfff762
Yoran9111 <Yoranvuranok@gmail.com>
2025-02-21 13:40:11 +0100

    Update hardware-configuration.nix

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -3,20 +3,5 @@
 {
   # This file is typically auto-generated during installation.
   # You should replace this with your actual hardware configuration.
-
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/bc60f8f7-7bcc-463a-86be-ca7984ab8154";
-    fsType = "ext4";
-  };
-
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/AF35-887B";
-    fsType = "vfat";
-  };
-
-  swapDevices = [
-    { device = "/dev/disk/by-uuid/58878902-de9a-468e-b4a3-a1f1effa6e"; }
-  ];
-
   hardware.cpu.intel.updateMicrocode = lib.mkDefault true;
 }
