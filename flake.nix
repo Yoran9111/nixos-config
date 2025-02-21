7a405379312510c5fa1918a60537936f5c0545b5
Yoran9111 <Yoranvuranok@gmail.com>
2025-02-21 11:41:46 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,17 +1,18 @@
 {
-  description = "NixOS configuration flake";
+  description = "My NixOS Configuration";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, ... }: {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";  # Adjust to your architecture if needed
-      modules = [
-        ./configuration.nix
-        ./hardware-configuration.nix
-      ];
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations = {
+      x86_64-linux = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ ./configuration.nix ];
+      };
     };
   };
 }
