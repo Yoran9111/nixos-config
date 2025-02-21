eb8a420b668b25fdaa0f7e6500a156e69b3055b3
Yoran9111 <Yoranvuranok@gmail.com>
2025-02-21 12:24:27 +0100

    Update hardware-configuration.nix

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -4,11 +4,19 @@
   # This file is typically auto-generated during installation.
   # You should replace this with your actual hardware configuration.
 
-  # Example (DO NOT USE AS-IS):
-  # fileSystems."/" = {
-  #   device = "/dev/disk/by-uuid/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx";
-  #   fsType = "ext4";
-  # };
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/bc60f8f7-7bcc-463a-86be-ca7984ab8154";
+    fsType = "ext4";
+  };
 
-  # hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/AF35-887B";
+    fsType = "vfat";
+  };
+
+  swapDevices = [
+    { device = "/dev/disk/by-uuid/58878902-de9a-468e-b4a3-a1f1effa6e"; }
+  ];
+
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault true;
 }
