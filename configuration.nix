105f4a0706164e075c8f4d1a10184a28e74a90e3
Yoran9111 <Yoranvuranok@gmail.com>
2025-02-25 09:40:46 +0100

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -51,24 +51,6 @@
     firefox
     nginxShibboleth
   ];
-
-  # Enable Nginx with Reverse Proxy
-  services.nginx = {
-    enable = true;
-    virtualHosts."mywebsite.com" = {
-      root = "/nix/store/79dljmcihdrv2bcrgp1imms81akxh599-nginx-1.26.3/html";
-      locations."/" = {
-        proxyPass = "http://192.168.254.134:30080/";
-        extraConfig = ''
-          index index.html;
-          proxy_set_header Host $host;
-          proxy_set_header X-Real-IP $remote_addr;
-          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        '';
-      };
-    };
-  };
-
   # Enable OpenSSH for remote access
   services.openssh.enable = true;
 
