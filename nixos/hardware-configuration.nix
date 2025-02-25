384ebf7e6f85efdd9fc968d93a75e8076a756a2a
Yoran9111 <Yoranvuranok@gmail.com>
2025-02-25 11:24:16 +0100

    Delete nixos/hardware-configuration.nix

--- nixos/hardware-configuration.nix
+++
@@ -1,7 +0,0 @@
-{ config, pkgs, lib, ... }:
-
-{
-  # This file is typically auto-generated during installation.
-  # You should replace this with your actual hardware configuration.
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault true;
-}
