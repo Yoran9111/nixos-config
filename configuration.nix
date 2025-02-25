36b75f76646512ff607437d5cd12d3741e5c1da4
Yoran9111 <Yoranvuranok@gmail.com>
2025-02-25 10:25:17 +0100

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,62 +1,40 @@
-{ config, pkgs, lib, ... }:
+{ config, pkgs, ... }:
 
 {
-  # Import hardware configuration
-  imports = [ ./hardware-configuration.nix ];
+  # Use the GRUB 2 boot loader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  boot.loader.grub.device = "/dev/sda";
 
-  # Bootloader Configuration (GRUB)
-  boot.loader.grub = {
-    enable = true;
-    device = "/dev/sda";  # Adjust this based on your setup
-    useOSProber = true;
+  # Enable disk
+  fileSystems."/" = {
+    device = "/dev/disk/by-label/nixos";
+    fsType = "btrfs";
+    options = [ "discard" "compress=lzo" ];
   };
 
-  # Filesystem Configuration
-  fileSystems."/" = lib.mkForce {
-    device = "/dev/sda";  # The device for the root partition
-    fsType = "btrfs";  # Force btrfs for root filesystem
-    options = [ "discard" "compress=lzo" ];  # These options match your script
-  };
-
-  fileSystems."/boot" = {
-    device = "/dev/sda1";  # Assuming /dev/sda1 is your boot partition
-    fsType = "vfat";  # Standard vfat for boot partition
-  };
-
-  # Networking Configuration
-  networking = {
-    hostName = "nixos";
-    networkmanager.enable = true;
-  };
-
-  # Locale & Timezone
-  time.timeZone = "Europe/Amsterdam";
-  i18n.defaultLocale = "en_US.UTF-8";
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "nl_NL.UTF-8";
-    LC_IDENTIFICATION = "nl_NL.UTF-8";
-    LC_MEASUREMENT = "nl_NL.UTF-8";
-    LC_MONETARY = "nl_NL.UTF-8";
-    LC_NAME = "nl_NL.UTF-8";
-    LC_NUMERIC = "nl_NL.UTF-8";
-    LC_PAPER = "nl_NL.UTF-8";
-    LC_TELEPHONE = "nl_NL.UTF-8";
-    LC_TIME = "nl_NL.UTF-8";
-  };
-
-  # Minimal Environment Configuration (No GUI)
+  # Install minimal packages needed for provisioning
   environment.systemPackages = with pkgs; [
-    wget
-    vim
-    firefox
-    nginxShibboleth
+    git
   ];
-  # Enable OpenSSH for remote access
+
+  # Turn off mutable users so `nixos-install` does not prompt to set a password
+  users.mutableUsers = false;
+
+  # Create my personal user
+  # The password should be changed later
+  users.extraUsers.kiba = {
+    description = "Jip";
+    isNormalUser = true;
+    initialPassword = "Jip";
+    extraGroups = [
+      "wheel"
+    ];
+    uid = 1000;
+  };
+
+  # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
-  # Enable Networking and firewall settings (SSH only for minimal access)
-  networking.firewall.allowedTCPPorts = [ 80 443 22 ];
-
-  # System Version (DO NOT CHANGE)
-  system.stateVersion = "24.11";
+  virtualisation.virtualbox.guest.enable = true;
 }
