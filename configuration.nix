25f5e0d9b4858f21dd29b9a02171426cfd959a65
Yoran9111 <Yoranvuranok@gmail.com>
2025-02-25 10:44:05 +0100

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,12 +1,12 @@
-{ config, pkgs, ... }:
+{ config, pkgs, ... }: 
 
 {
-  # Use the GRUB 2 boot loader.
+  # Bootloader Configuration (GRUB)
   boot.loader.grub.enable = true;
   boot.loader.grub.version = 2;
   boot.loader.grub.device = "/dev/sda";
 
-  # Enable disk
+  # Filesystem Configuration (BTRFS)
   fileSystems."/" = {
     device = "/dev/disk/by-label/nixos";
     fsType = "btrfs";
@@ -21,20 +21,18 @@
   # Turn off mutable users so `nixos-install` does not prompt to set a password
   users.mutableUsers = false;
 
-  # Create my personal user
-  # The password should be changed later
-  users.extraUsers.kiba = {
+  # Create the user "jip"
+  users.extraUsers.jip = {
     description = "Jip";
     isNormalUser = true;
     initialPassword = "Jip";
-    extraGroups = [
-      "wheel"
-    ];
+    extraGroups = [ "wheel" ];
     uid = 1000;
   };
 
-  # Enable the OpenSSH daemon.
+  # Enable OpenSSH for remote access
   services.openssh.enable = true;
 
+  # Enable VirtualBox guest additions (if using VirtualBox)
   virtualisation.virtualbox.guest.enable = true;
 }
