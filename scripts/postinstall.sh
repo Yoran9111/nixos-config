e6fdd91c03d0e97138873489f86fbe9aa016a2fc
Yoran9111 <Yoranvuranok@gmail.com>
2025-02-25 11:54:14 +0100

    Update postinstall.sh

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -8,11 +8,11 @@ UHOME=/home/$USER
 # Setup my configuration lib
 sudo -u $USER mkdir -p $UHOME/lib
 sudo -u $USER git clone https://github.com/Yoran9111/nixos.git $UHOME/lib/nixos
-sudo -u $USER git -C $UHOME/lib/nixos remote set-url --push origin git@github.com:KibaFox/nixos.git
+sudo -u $USER git -C $UHOME/lib/nixos remote set-url --push origin git@github.com:Yoran9111/nixos.git
 
 # Install nixos configuration
 mv /etc/nixos/configuration.nix /etc/nixos/bak.nix
-ln -s $UHOME/lib/nixos/machines/vbox.nix /etc/nixos/configuration.nix
+ln -s $UHOME/lib/nixos/machines/nixOS1.nix /etc/nixos/configuration.nix
 nixos-rebuild switch --upgrade
 
 # Cleanup any previous generations and delete old packages
